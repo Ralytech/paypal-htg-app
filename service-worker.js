4a08ca293826efd15b7c854dbe67e64f6b483000
Ralytech <ralytechdigitalagency@gmail.com>
2025-09-13 23:26:57 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,9 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('paypal-htg-cache').then(function(cache) {
-      return cache.addAll([
-        'index.html',
-        'manifest.json',
-        'icon-192.png',
-        'icon-512.png'
-      ]);
-    })
-  );
+const CACHE='lakay-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))))
 });
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)))
 });
